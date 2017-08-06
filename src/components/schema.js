66621f639cb3d11de9c9ead1f0126146872d7cb3
Janis Miezitis <janjiss@gmail.com>
2017-08-06 23:55:57 +0300

    Added marks and nodes for full text editor features

--- src/components/schema.js
+++ src/components/schema.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import transformations from './schemaRules';
-
 const schema = {
 	nodes: {
 		paragraph: props => {
@@ -16,9 +15,73 @@ const schema = {
 					{props.children}
 				</h1>
 			);
+		},
+		headerOne: props => {
+			return (
+				<h1 {...props.attributes}>
+					{props.children}
+				</h1>
+			);
+		},
+		headerTwo: props => {
+			return (
+				<h2 {...props.attributes}>
+					{props.children}
+				</h2>
+			);
+		},
+		headerThree: props => {
+			return (
+				<h3 {...props.attributes}>
+					{props.children}
+				</h3>
+			);
+		},
+		listItem: props => {
+			return (
+				<li {...props.attributes}>
+					{props.children}
+				</li>
+			);
+		},
+		unorderedLIst: props => {
+			return (
+				<ul {...props.attributes}>
+					{props.children}
+				</ul>
+			);
+		},
+		orderedList: props => {
+			return (
+				<ol {...props.attributes}>
+					{props.children}
+				</ol>
+			);
 		}
 	},
-	marks: [],
+	marks: {
+		bold: props => {
+			return (
+				<strong {...props.attributes}>
+					{props.children}
+				</strong>
+			);
+		},
+		italic: props => {
+			return (
+				<i {...props.attributes}>
+					{props.children}
+				</i>
+			);
+		},
+		underline: props => {
+			return (
+				<u {...props.attributes}>
+					{props.children}
+				</u>
+			);
+		}
+	},
 	rules: transformations
 };
 
